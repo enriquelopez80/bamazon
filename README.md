18e2742b77c6ac87e0c22f48c526e853f35c96f0
Rico <enriquelopez80@gmail.com>
2018-08-04 19:00:52 -0700

    Final Push

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # bamazon
+
+###App Description:
+
+This application is a command line based storefront that uses the [Inquirer](https://www.npmjs.com/package/inquirer) NPM package and a MySQL database together with a [MySQL](https://www.npmjs.com/package/mysql) NPM package. The app takes in orders from customers and depletes stock from the store's inventory.
+
+
+###Interface:
+
+When running the app, a customer is first given all of the items available for sale including the ids, names,prices and stock amount of products for sale. The user is then given two prompts: first asking what item they would like to buy then how many they want to purchase.
+
+If the selected quantity is in stock, the user's order is fulfilled and they are shown the item they chose plus the cost of that item. They're also showed the new stock amount plus that number is updated in the database.
+
+If the desired quantity isn't available, the user is prompted to modify their order.
+
+##Demo:
+
+Watch the demo to the bamazon customer interface [here.](https://drive.google.com/file/d/1gYyD06qjvUDW1RP7c4SJxvcaeKh-wDZI/view)
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,7 +1,8 @@
+//Dependencies
 const mysql = require("mysql");
 const inquirer = require("inquirer");
 
-//create connection with bamazon
+//Create connection with bamazon database
 let connection = mysql.createConnection({
     host: 'localhost',
     port: 3306,
@@ -17,23 +18,25 @@ connection.connect(function (err) {
     queryAllProducts();
 });
 
+//Display all products
 function queryAllProducts() {
     connection.query("SELECT * FROM products", function (err, res) {
         if (err) throw err;
         res.forEach(function (product) {
             console.log(`
-            Product:
-            =========================
-            ID: ${product.item_id}
-            NAME: ${product.product_name}
-            PRICE: $${product.price}
-            STOCK: ${product.stock_quantity}
-            `)
+                    Product:
+                    ==========================
+                    ID: ${product.item_id}
+                    NAME: ${product.product_name}
+                    PRICE: $${product.price}
+                    STOCK: ${product.stock_quantity}
+                    `)
         })
         productSearch()
     })
 }
 
+//Prompt user
 function productSearch() {
     inquirer.prompt([{
             name: "item_id",
@@ -51,45 +54,65 @@ function productSearch() {
         let item = input.item_id;
         let quantity = input.stock_quantity;
 
+        //Select item
         connection.query('SELECT * FROM products WHERE ?', {
             item_id: item
         }, function (err, res) {
             if (err) throw err;
             res.forEach(function (product) {
                 console.log(`
-            Product:
-            =========================
-            ID: ${product.item_id}
-            NAME: ${product.product_name}
-            PRICE: $${product.price}
-            STOCK: ${product.stock_quantity}
+                    Product:
+                    ==========================
+                    ID: ${product.item_id}
+                    NAME: ${product.product_name}
+                    PRICE: $${product.price}
+                    STOCK: ${product.stock_quantity}
             `)
 
                 let itemRes = res[0]
-
+                
+                //Stock availability and cost
                 if (quantity <= itemRes.stock_quantity) {
-                    console.log('Looks like we have enough!')
+                    console.log(`
+                    ==========================
+                    Looks like we have enough!
+                    ==========================
+                    `)
 
                     let transactionCost = (product.price * quantity)
+                    let newStock = (product.stock_quantity - quantity)
 
-                    console.log("Total cost of purchase: $ " + transactionCost)
+                    console.log(`
+                    ==========================
+                    Total cost: $${transactionCost}
+                    ==========================
+                    `)
+                    console.log(`
+                    ==========================
+                    New stock amount: ${newStock}
+                    ==========================
+                    `)
+
+                    //Update database
+                    connection.query('UPDATE products SET ? WHERE ?', [{
+                            stock_quantity: product.stock_quantity - quantity
+                        },
+                        {
+                            item_id: item
+                        }
+                    ], function (err, res) {
+                        if (err) throw err;
+                    })
 
                 } else {
-                    console.log("Sorry we don't have enough.")
-                    productSearch()
+                    console.log(`
+                    ==========================
+                    Sorry we don't have enough.
+                    Please modify your order.
+                    ==========================
+                            `)
                 }
-
-                connection.query('UPDATE products SET ? WHERE ?', [{
-                        stock_quantity: product.stock_quantity - quantity
-                    },
-                    {
-                        item_id: item
-                    }
-                ], function (err, res) {
-                    if (err) throw err;
-                    
-                    productSearch()
-                })
+                productSearch()
             })
         })
     })
